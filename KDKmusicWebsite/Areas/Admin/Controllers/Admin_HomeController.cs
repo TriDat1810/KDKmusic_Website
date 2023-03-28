4366f592f59ce0fbe6bb8c3351b411a6fabf3b81
Kienpro10082002 <Zuka10082002@gmail.com>
2023-03-28 23:14:41 +0700

    Areas for admin

---
+++ KDKmusicWebsite/Areas/Admin/Controllers/Admin_HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KDKmusicWebsite.Areas.Admin.Controllers
+{
+    public class Admin_HomeController : Controller
+    {
+        // GET: Admin/Admin_Home
+        public ActionResult AdminIndex()
+        {
+            return View();
+        }
+    }
+}
---
+++ KDKmusicWebsite/Controllers/ArtistController.cs
@@ -0,0 +1,30 @@
+﻿using KDKmusicWebsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KDKmusicWebsite.Controllers
+{
+    public class ArtistController : Controller
+    {
+        DBkdkMusicModelDataContext data = new DBkdkMusicModelDataContext();
+        // GET: Artist
+        public ActionResult ShowDisplay()
+        {
+            var showlist = data.Artists.ToList();
+            return View(showlist);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var details = data.Artists.FirstOrDefault(a => a.Artist_Id == id);
+            if(details == null)
+            {
+                return HttpNotFound();
+            }
+            return View(details);
+        }
+    }
+}
